4ec594515901b1c33bf54d53e64c61bfb3bd6cf0
hector1234567 <52801178+hector1234567@users.noreply.github.com>
2026-06-23 15:19:06 +0200

    Cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,53 @@
 import "./App.css";
+import StatCard from "./components/stat-card";
+
+import facebook from "../images/icon-facebook.svg";
+import twitter from "../images/icon-twitter.svg";
+import instagram from "../images/icon-instagram.svg";
+import youtube from "../images/icon-youtube.svg";
 
 function App() {
   return (
-    <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="bg-gray-950 min-h-screen font-display">
+      <div className="bg-gray-900 rounded-b-3xl p-4">
+        <div className="container mx-auto">
+          <h1 className="text-white font-bold text-xl">
+            Social Media Dashboard
+          </h1>
+          <p className="text-gray-400">Total Followers: 23,004</p>
+        </div>
+      </div>
+      <div className="container mx-auto p-4 space-y-4">
+        <StatCard
+          icon={facebook}
+          author="nathanf"
+          followers="1987"
+          qty="12"
+          gradient="from-blue-500 to-blue-500"
+        />
+        <StatCard
+          icon={twitter}
+          author="nathanf"
+          followers="1044"
+          qty="12"
+          gradient="from-blue-500 to-blue-500"
+        />
+        <StatCard
+          icon={instagram}
+          author="nathanf"
+          followers="11k"
+          qty="12"
+          gradient="from-gradient-11 via-gradient-12 to-gradient-13"
+        />
+        <StatCard
+          icon={youtube}
+          author="nathanf"
+          followers="8239"
+          qty="12"
+          gradient="from-red-700 to-red-700"
+        />
+      </div>
+    </div>
   );
 }
 
---
+++ src/components/stat-card.jsx
@@ -0,0 +1,15 @@
+export default function StatCard({ icon, author, followers, qty, gradient }) {
+  return (
+    <div className={`bg-gradient-to-r ${gradient} pt-1 rounded-sm`}>
+      <div className={`bg-navy-950 p-5 text-center`}>
+        <div className="text-gray-400 flex justify-center text-xs mb-3">
+          <img src={icon} alt="Icon" className="mr-2 self-center" />
+          <span className=" self-center font-bold">@{author}</span>
+        </div>
+        <p className="text-white font-extrabold text-5xl mb-1">{followers}</p>
+        <p className="text-gray-400 text-xs mb-3 tracking-[.3rem]">Followers</p>
+        <p className="text-green-500">{qty} Today</p>
+      </div>
+    </div>
+  );
+}
