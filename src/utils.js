f88f09a0045bcef0fa68f6f0582611dba2352406
hector1234567 <52801178+hector1234567@users.noreply.github.com>
2026-06-27 22:05:15 +0200

    zod

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,36 @@
+import { z } from "zod";
+
+const statSchema = z
+  .array(
+    z.object({
+      socialApp: z.string(),
+      author: z.string(),
+      followers: z.string(),
+      qty: z.number(),
+    }),
+  )
+  .default([]);
+
 export async function fetchStats() {
   const res = await fetch("/stats.json");
-  return await res.json();
+  const data = await res.json();
+  return statSchema.parse(data);
 }
+
+const overviewSchema = z
+  .array(
+    z.object({
+      title: z.string(),
+      qty: z.string(),
+      socialApp: z.string(),
+      percentage: z.number(),
+      rising: z.boolean(),
+    }),
+  )
+  .default([]);
 
 export async function fetchOverviews() {
   const res = await fetch("/overviews.json");
-  return await res.json();
+  const data = await res.json();
+  return overviewSchema.parse(data);
 }
